fb7697038ff335d81d85c9b48eb6859e4c6a7a11
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-09-11 16:48:37 +0100

    Add workspaces data to shared props in HandleInertiaRequests middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,10 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'workspaces' => fn () => $request->user()
+                ?->workspaces()
+                ->select('workspaces.id', 'workspaces.name')
+                ->get(),
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
     }
