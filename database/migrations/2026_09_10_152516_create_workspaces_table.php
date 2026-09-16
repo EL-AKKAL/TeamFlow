33d6ce2e2b0e22eb7b1f595b000b866abdcaafb1
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-09-16 15:52:06 +0100

    remove unnecessary description field from workspace

--- database/migrations/2026_09_10_152516_create_workspaces_table.php
+++ database/migrations/2026_09_10_152516_create_workspaces_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
             $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
