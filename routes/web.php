4bb084d96707c4542062cb9714c0c2ab6ff505ca
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-09-22 17:44:16 +0000

    add MemberController with basic CRUD methods and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MemberController;
 use App\Http\Controllers\WorkspaceController;
 use Illuminate\Support\Facades\Route;
 
