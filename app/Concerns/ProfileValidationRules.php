5076134b26d874e143fe74ae718ff26ae0389ce3
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-09-24 15:35:30 +0000

    add avatar validation rule

--- app/Concerns/ProfileValidationRules.php
+++ app/Concerns/ProfileValidationRules.php
@@ -3,39 +3,24 @@
 namespace App\Concerns;
 
 use App\Models\User;
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Validation\Rule;
 
 trait ProfileValidationRules
 {
-    /**
-     * Get the validation rules used to validate user profiles.
-     *
-     * @return array<string, array<int, ValidationRule|array<mixed>|string>>
-     */
     protected function profileRules(?int $userId = null): array
     {
         return [
             'name' => $this->nameRules(),
             'email' => $this->emailRules($userId),
+            'avatar' => $this->avatarRules(),
         ];
     }
 
-    /**
-     * Get the validation rules used to validate user names.
-     *
-     * @return array<int, ValidationRule|array<mixed>|string>
-     */
     protected function nameRules(): array
     {
         return ['required', 'string', 'max:255'];
     }
 
-    /**
-     * Get the validation rules used to validate user emails.
-     *
-     * @return array<int, ValidationRule|array<mixed>|string>
-     */
     protected function emailRules(?int $userId = null): array
     {
         return [
@@ -47,5 +32,10 @@ trait ProfileValidationRules
                 ? Rule::unique(User::class)
                 : Rule::unique(User::class)->ignore($userId),
         ];
+    }
+
+    protected function avatarRules(): array
+    {
+        return ['nullable', 'image', 'max:2048'];
     }
 }
